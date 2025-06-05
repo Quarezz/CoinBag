cc82650aefed3ae9ed07262fd6c1842384155182
Ruslan <morfed.quarezz@gmail.com>
2025-06-05 23:20:28 +0300

    expences doen

--- coinbag_flutter/lib/domain/repositories/expense/expense_repository.dart
+++ coinbag_flutter/lib/domain/repositories/expense/expense_repository.dart
@@ -1,11 +1,7 @@
 import 'package:coinbag_flutter/data/models/expense.dart';
 
 abstract class ExpenseRepository {
-  Future<List<Expense>> fetchExpenses({
-    required String accountId,
-    int page = 0,
-    int pageSize = 20,
-  });
+  Future<List<Expense>> fetchExpenses({int page = 0, int pageSize = 20});
   Future<void> addExpense(Expense expense);
   Future<void> removeExpense(String id);
   Future<void> editExpense(Expense expense);
